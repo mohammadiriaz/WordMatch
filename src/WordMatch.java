6576e99935abcd451720242785bddd29723ac975
mohammadiriaz <mohammadr95@nycstudents.net>
2025-10-14 12:34:17 -0400

    Update WordMatch.java
    Didn't initially save all code.

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+public class WordMatch{
+    private String secret;
 
-public class WordMatch {
+    public WordMatch(String word)
+    {
+        secret = word;
+    }
+
+    public int scoreGuess(String guess) {
+        int score = 0;
+
+        for(int i = 0; i <= secret.length() - guess.length(); i++)
+            if (secret.substring(i, i + guess.length()).equals(guess))
+            {
+                score++;
+            }
+        return score * guess.length() * guess.length();
+        }
+
+
+
+
+
+    public  String findBetterGuess(String guess1, String guess2)
+    {
+        return null;
+    }
 }
