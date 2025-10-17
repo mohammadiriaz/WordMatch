c78cfa7aeecfcfecd99dfea0d31d7d79f4550cc2
MohammadRiaz <mohammadr95@nycstudents.net>
2025-10-17 12:25:47 -0400

    Finished task with all test cases.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+
+        WordMatch Test1 = new WordMatch("aaaabb");
+        System.out.println(Test1.scoreGuess("a"));
+        System.out.println(Test1.scoreGuess("aa"));
+        System.out.println(Test1.scoreGuess("aaa"));
+        System.out.println(Test1.scoreGuess("aabb"));
+        System.out.println(Test1.scoreGuess("c"));
+
+        WordMatch Test2 = new WordMatch("concatenation");
+        System.out.println(Test2.scoreGuess("ten"));
+        System.out.println(Test2.scoreGuess("nation"));
+        System.out.println(Test2.findBetterGuess("ten", "nation"));
+        System.out.println(Test2.scoreGuess("con"));
+        System.out.println(Test2.scoreGuess("cat"));
+        System.out.println(Test2.findBetterGuess("con", "cat"));
     }
 }
